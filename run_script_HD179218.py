87f81784537d073ce77a653ccca8edba63f2cf0d
IainHammond <63645203+IainHammond@users.noreply.github.com>
2020-09-14 20:11:41 +1000

    support for cropping and binning

--- run_script_HD179218.py
+++ run_script_HD179218.py
@@ -5,18 +5,17 @@ Created on Sun Apr 12 13:11:12 2020
 
 @author: lewis, iain
 """
-#classification stage already completed
 
 from naco_pip import input_dataset, raw_dataset, calib_dataset
 
-clas = input_dataset('/home/ihammond/pd87_scratch/products/NACO_archive/5_HD179218/raw/','/home/ihammond/pd87_scratch/products/IainData/HD179218/classified/')
+clas = input_dataset('/home/ihammond/pd87_scratch/products/NACO_archive/5_HD179218/raw/','/home/ihammond/pd87_scratch/products/NACO_archive/5_HD179218/classified/', coro = True)
 
 #clas.bad_columns()
 #clas.mk_dico()
 #clas.find_sky_in_sci_cube(plot = 'save')
 #clas.find_derot_angles()
 
-calib = raw_dataset('/home/ihammond/pd87_scratch/products/IainData/HD179218/classified/','/home/ihammond/pd87_scratch/products/IainData/HD179218/calibrated/')
+calib = raw_dataset('/home/ihammond/pd87_scratch/products/NACO_archive/5_HD179218/classified/','/home/ihammond/pd87_scratch/products/NACO_archive/5_HD179218/calibrated/', final_sz = None)
 
 #calib.dark_subtract(debug = False, plot = False)
 ##calib.fix_sporatic_columns(quadrant='topleft', xpixels_from_center = 7, interval = 8, verbose = True, debug = False)
@@ -27,8 +26,9 @@ calib = raw_dataset('/home/ihammond/pd87_scratch/products/IainData/HD179218/clas
 #calib.get_stellar_psf(debug = False, plot = False)
 #calib.subtract_sky(debug = False, plot = False)
 
-preproc = calib_dataset('/home/ihammond/pd87_scratch/products/IainData/HD179218/calibrated/','/home/ihammond/pd87_scratch/products/IainData/HD179218/preprocessed/', 47, '2dfit', '2gauss', calib_npcs = 1, coro=True)
+preproc = calib_dataset('/home/ihammond/pd87_scratch/products/NACO_archive/5_HD179218/calibrated/','/home/ihammond/pd87_scratch/products/NACO_archive/5_HD179218/preproc/', 47, 'speckle', 'gauss', coro=True)
 
 #preproc.recenter(nproc = 1, sigfactor = 4, subi_size = 21,verbose = True, debug = False, plot = False, coro = True)
-preproc.bad_frame_removal(correlation_thres = 0.90, pxl_shift_thres = 0.5, crop_size = 31, verbose = True, debug = False, plot = False)
-#preproc.median_binning(binning_factor = 1)
+#preproc.bad_frame_removal(correlation_thres = 0.95, pxl_shift_thres = 0.5, crop_size = 31, verbose = True, debug = False, plot = 'save')
+#preproc.crop_cube(arcsecond_diameter = 3, verbose = True, full_output= False)
+preproc.median_binning(binning_factor = [1,5,25], verbose = True)
