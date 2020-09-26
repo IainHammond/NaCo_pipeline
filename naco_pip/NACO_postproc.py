bbd8fdc7f23c0fcbeb6e9c94ecc6cf74a505ae4d
IainHammond <63645203+IainHammond@users.noreply.github.com>
2020-09-26 16:07:53 +1000

    further additions

--- naco_pip/NACO_postproc.py
+++ naco_pip/NACO_postproc.py
@@ -22,8 +22,10 @@ import vip_hci as vip
 from vip_hci.fits import open_fits, write_fits
 from vip_hci.pca import pca, pca_annular
 from vip_hci.metrics import normalize_psf
+from naco_pip import fits_info
 
 # Definition of Useful function
+
 
 def find_nearest(array, value, output='index', constraint=None):
     """
@@ -58,55 +60,117 @@ class postproc_dataset():  #this class is for post-processing of the pre-process
         self.outpath = outpath
         self.nproc = nproc
         self.npc = npc
+        self.fwhm = open_fits(self.inpath + 'fwhm.fits')[0] # fwhm is first entry
+        if verbose:
+            print('FWHM:',self.fwhm)
+        self.plsc_ori = fits_info.pixel_scale       
 
-    def pca(self, recenter_method, recenter_model, do_adi, do_pca_full, do_pca_ann, verbose = True, debug = False):
-             
-        ######################### Simple ADI ###########################
+
+    def pca(self, recenter_method, recenter_model, cropped = True, binning_factor, do_adi = True, do_pca_full = True, do_pca_ann = False, do_snr_map = True, do_snr_map_opt = True, delta_rot = (0.5,3), verbose = True, debug = False):        
+        """ 
+        For post processing the master cube using PCA-ADI        
+        
+        Parameters:
+        ***********        
+        recenter_method : str
+            method used to recenter in preproc
+        recenter_model : str
+            model used to recenter in preproc
+        
+        cropped : bool
+            whether to use cropped frames. Must be used for running PCA in concentric annuli 
+            
+        binning_factor : int
+            for use in the name of the master cube. will not be considered if cropped = False
+        
+        do_adi : bool
+            Whether to do a median-ADI processing
+        
+        do_pca_full : bool
+            Whether to apply PCA-ADI on full frame
+        
+        do_pca_ann : bool, default is False
+            Whether to apply PCA-ADI in concentric annuli (more computer intensive)
+        
+        do_snr_map : bool
+            whether to compute an SNR map (warning: computer intensive); useful only when point-like features are seen in the image
+            
+        do_snr_map_opt : bool
+            Whether to compute a non-conventional (more realistic) SNR map 
+            
+        delta_rot : tuple
+            Threshold in rotation angle used in pca_annular to include frames in the PCA library (provided in terms of FWHM). See description of pca_annular() for more details
+        
+        verbose : bool
+            prints more output when True                 
+ 
+        """
+        details = fits_info.details
+        source = fits_info.source
+        ADI_cube_name = 'master_cube_{}_{}_good_frames_cropped_bin{}.fits'                 # template name for input master cubes (i.e. the recentered bad-frame trimmed ones)
+        derot_ang_name = 'derot_angles_{}_{}_good_frames_bin{}.fits'                       # template name for corresponding input derotation angles 
+        psf_name = "master_unsat_psf.fits"                                                 # name of the non-coroangraphic stellar PSF
+        psfn_name = "master_unsat_psf_norm.fits" 
+        mask_IWA = 0.5
+        mask_IWA_px = int(mask_IWA*self.fwhm)
+        print("adopted mask size: {:.0f}".format(mask_IWA_px))
+        tn_shift = -0.58                                                                      # size of numerical mask hiding the inner part of post-processed images. Provided in terms of fwhm.
+        outpath_sub = self.outpath+"Sub_bin{}_{}_{}/"
+        
+        ann_sz=3                                                                       # if PCA-ADI in a single annulus or in concentric annuli, this is the size of the annulus/i in FWHM
+        svd_mode_sann = 'lapack'                                                       # python package used for Singular Value Decomposition in PCA-ADI sann
+        svd_mode_1 = 'lapack'                                                          # python package used for Singular Value Decomposition for PCA reductions on uncropped cube
+        svd_mode_2 = 'lapack'                                                          # python package used for Singular Value Decomposition for PCA reductions on cropped cube (Note: 'lapack' is more robust but 'randsvd' is significantly faster)        
+        svd_mode_all = [svd_mode_1,svd_mode_2]                                         
+        n_randsvd = 3                                                                  # if svd package is set to 'randsvd' number of times we do PCA rand-svd, before taking the median of all results (there is a risk of significant self-subtraction when just doing it once)
+        ref_cube = None                                                                # if any, load here a centered calibrated cube of reference star observations - would then be used for PCA instead of the SCI cube itself 
+
+        # Overwrite?
+        overwrite_ADI = True                                                           # whether to overwrite output median-ADI files
+        overwrite_pp = False         
+        
+        # TEST number of principal components - for cropped and uncropped cubes
+        ## PCA-FULL       
+        test_pcs_full_crop = list(range(1,16))
+        test_pcs_full_nocrop = list(range(1,16))
+        test_pcs_full_all = [test_pcs_full_crop,test_pcs_full_nocrop]
+        # PCA-ANN
+        test_pcs_ann_crop = list(range(1,16))
+        test_pcs_ann_nocrop = list(range(1,16))
+        test_pcs_ann_all = [test_pcs_ann_crop, test_pcs_ann_nocrop]
+        
+        if not isdir(self.outpath):
+            os.system("mkdir "+self.outpath)
+        if not isdir(outpath_sub.format(binning_factor,recenter_method, recenter_model)):
+            os.system("mkdir "+outpath_sub.format(binning_factor,recenter_method, recenter_model))            
+        
+        oversamp_fac = 1
+        plsc = plsc_ori/oversamp_fac
+        
+        ADI_cube = open_fits(self.inpath+ADI_cube_name.format(recenter_method, recenter_model,binning_factor))
+        derot_angles = open_fits(self.inpath+derot_ang_name.format(recenter_method, recenter_model,binning_factor))+tn_shift
+        psf = open_fits(self.inpath+psf_name)
+        psfn, starphot, fwhm = normalize_psf(psf, fwhm='fit', size=None, threshold=None, mask_core=None,
+                                             model='gauss', imlib='opencv', interpolation='lanczos4',
+                                             force_odd=True, full_output=True, verbose=verbose, debug=debug)        
+        
         if do_adi:
-            if not isfile(outpath_sub.format(bin_fac,cen_met)+'final_ADI_simple'+label+label_ov+label_test+'.fits') or overwrite_ADI or debug:
+            if not isfile(outpath_sub.format(binning_factor,recenter_method,recenter_model)+'final_ADI_simple.fits') or overwrite_ADI or debug:
                 if debug:
-                    tmp, _, tmp_tmp = vip.medsub.median_sub(ADI_cube, derot_angles, fwhm=fwhm,
+                    tmp, _, tmp_tmp = vip.medsub.median_sub(ADI_cube, derot_angles, fwhm=self.fwhm,
                                              radius_int=0, asize=ann_sz, delta_rot=delta_rot, 
-                                             full_output=debug, verbose=True)
-                    tmp = vip.var.mask_circle(tmp,0.9*fwhm)
-                    write_fits(outpath_sub.format(bin_fac,cen_met)+'TMP_ADI_simple_cube_der'+label+label_ov+label_test+'.fits', tmp)
+                                             full_output=debug, verbose=verbose)
+                    tmp = vip.var.mask_circle(tmp,0.9*self.fwhm)
+                    write_fits(outpath_sub.format(binning_factor,recenter_method,recenter_model)+'TMP_ADI_simple_cube_der.fits', tmp)
                 else:
-                    tmp_tmp = vip.medsub.median_sub(ADI_cube, derot_angles, fwhm=fwhm,
+                    tmp_tmp = vip.medsub.median_sub(ADI_cube, derot_angles, fwhm=self.fwhm,
                                              radius_int=0, asize=ann_sz, delta_rot=delta_rot, 
-                                             full_output=debug, verbose=True)
-                tmp_tmp = vip.var.mask_circle(tmp_tmp,mask_IWA*fwhm)  # we mask the IWA
-                write_fits(outpath_sub.format(bin_fac,cen_met)+'final_ADI_simple'+label+label_ov+label_test+'.fits', tmp_tmp)
+                                             full_output=debug, verbose=verbose)
+                tmp_tmp = vip.var.mask_circle(tmp_tmp,mask_IWA*self.fwhm)  # we mask the IWA
+                write_fits(outpath_sub.format(binning_factor,recenter_method,recenter_model)+'final_ADI_simple.fits', tmp_tmp)
             else:
-                tmp_tmp = open_fits(outpath_sub.format(bin_fac,cen_met)+'final_ADI_simple'+label+label_ov+label_test+'.fits')
-            #id_snr_adi_df[counter] = vip.metrics.snr(tmp_tmp, (xx_comp,yy_comp), fwhm)
-            ## Convolution
-            if (not isfile(outpath_sub.format(bin_fac,cen_met)+'final_ADI_simple'+label+label_ov+label_test+'_conv.fits') or overwrite_ADI) and do_conv:
-                tmp = open_fits(outpath_sub.format(bin_fac,cen_met)+'final_ADI_simple'+label+label_ov+label_test+'.fits')
-                tmp = vip.var.frame_filter_gaussian2d(tmp, fwhm/2, mode='conv')
-                write_fits(outpath_sub.format(bin_fac,cen_met)+'final_ADI_simple'+label+label_ov+label_test+'_conv.fits', tmp, verbose=False)
-            ## SNR map  
-            if (not isfile(outpath_sub.format(bin_fac,cen_met)+'final_ADI_simple'+label+label_ov+label_test+'_snrmap.fits') or overwrite_ADI) and do_snr_map:
-                tmp = open_fits(outpath_sub.format(bin_fac,cen_met)+'final_ADI_simple'+label+label_ov+label_test+'.fits')
-                rad_in = mask_IWA 
-                tmp = vip.var.mask_circle(tmp,rad_in*fwhm)
-                tmp_tmp = vip.metrics.snrmap(tmp, fwhm)
-                write_fits(outpath_sub.format(bin_fac,cen_met)+'final_ADI_simple'+label+label_ov+label_test+'_snrmap.fits', tmp_tmp, verbose=False)
-            ## Contrast curve ADI
-            if do_adi_contrast:
-                #psfn = open_fits(outpath_3+'master_unsat_psf_norm'+label_ov+'.fits')
-                #starphot = open_fits(inpath+'fluxes.fits')[ii]
-                pn_contr_curve_adi = vip.metrics.contrast_curve(ADI_cube, derot_angles, psfn,
-                                                             fwhm, plsc, starphot=starphot, 
-                                                             algo=vip.madi.adi, sigma=5., nbranch=1, 
-                                                             theta=0, inner_rad=1, wedge=(0,360),
-                                                             student=True, transmission=None, smooth=True,
-                                                             plot=False, dpi=100, debug=False, 
-                                                             verbose=verbose)
-                arr_dist_adi = np.array(pn_contr_curve_adi['distance'])
-                arr_contrast_adi = np.array(pn_contr_curve_adi['sensitivity (Student)'])
-                for ff in range(nfcp):
-                    idx = find_nearest(arr_dist_adi, rad_arr[ff])
-                    sensitivity_5sig_adi_df[ff] = arr_contrast_adi[idx]
-            ADI_cube = None 
-
-
+                tmp_tmp = open_fits(outpath_sub.format(binning_factor,recenter_method,recenter_model)+'final_ADI_simple.fits')        
+        
+        
+        
+                
