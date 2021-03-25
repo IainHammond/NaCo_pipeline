8fc20cfcb498e1b7ee9a86a3b55794e0e39d4114
iainhammond <imham3@student.monash.edu>
2021-03-25 21:08:09 +1100

    prelim negfc

--- naco_pip/NACO_postproc.py
+++ naco_pip/NACO_postproc.py
@@ -16,8 +16,8 @@ from vip_hci.fits import open_fits, write_fits
 from vip_hci.pca import pca, pca_annular
 from vip_hci.metrics import snrmap,contrast_curve
 from vip_hci.medsub import median_sub
-from vip_hci.var import mask_circle,frame_filter_lowpass
-from vip_hci.negfc import mcmc_negfc_sampling, show_corner_plot, show_walk_plot, confidence, firstguess, cube_planet_free
+from vip_hci.var import mask_circle,frame_filter_lowpass, frame_center
+from vip_hci.negfc import mcmc_negfc_sampling, firstguess
 
 class preproc_dataset:  #this class is for post-processing of the pre-processed data
     def __init__(self,inpath,outpath,dataset_dict,nproc,npc):
@@ -279,59 +279,104 @@ class preproc_dataset:  #this class is for post-processing of the pre-processed 
                    tmp[pp] = mask_circle(tmp[pp], mask_IWA_px)
                write_fits(outpath_sub +'final_PCA-ADI_ann_'+test_pcs_str+'_snrmap_opt.fits',tmp, verbose=verbose)
 
-    # def do_negfc(self,guess_xy=(0,0),mcmc_negfc=True,verbose=True,debug=False):
-    #     """
-    #     xxx
-    #
-    #     Parameters:
-    #     ***********
-    #     guess : tuple
-    #         first estimate of the source location to be provdied to firstguess()
-    #     mcmc_negfc : bool
-    #         whether to run MCMC NEGFC sampling (computationally intensive)
-    #     verbose : bool
-    #         prints more output when True
-    #
-    #     """
-    #     print("======= Starting NEGFC....=======")
-    #
-    #     outpath_sub = self.outpath + "negfc/"
-    #
-    #     if not isdir(self.outpath):
-    #         os.system("mkdir " + self.outpath)
-    #     if not isdir(outpath_sub):
-    #         os.system("mkdir " + outpath_sub)
-    #
-    #     if verbose:
-    #         print('Input path is {}'.format(self.inpath))
-    #         print('Output path is {}'.format(outpath_sub))
-    #
-    #     source = self.dataset_dict['source']
-    #
-    #     ADI_cube_name = '{}_master_cube.fits'  # template name for input master cube
-    #     derot_ang_name = 'derot_angles.fits'  # template name for corresponding input derotation angles
-    #     ADI_cube = open_fits(self.inpath + ADI_cube_name.format(source), verbose=verbose)
-    #     derot_angles = open_fits(self.inpath + derot_ang_name, verbose=verbose) + tn_shift
-    #
-    #     ini_state = firstguess(ADI_cube, derot_angles, psfn, ncomp=opt_npc[ff], plsc=plsc,
-    #                            planets_xy_coord=planet_pos_crop, fwhm=fwhm,
-    #                            annulus_width=asize, aperture_radius=ap_rad, cube_ref=ref_cube,
-    #                            svd_mode=svd_mode, scaling=None, fmerit=fm, imlib=imlib,
-    #                            interpolation=interpolation, collapse='median', p_ini=None,
-    #                            transmission=transmission, algo=algo,
-    #                            f_range=f_range, simplex=True, simplex_options=None, plot=False,
-    #                            verbose=True, save=False)
-    #
-    #     final_chain = mcmc_negfc_sampling(ADI_cube, derot_angles, psfn, ncomp=opt_npc[ff], plsc=plsc,
-    #                                       initial_state=ini_state, fwhm=fwhm, weights=weights,
-    #                                       annulus_width=asize, aperture_radius=ap_rad, cube_ref=ref_cube,
-    #                                       svd_mode=svd_mode, scaling=None, fmerit=fm,
-    #                                       imlib=imlib, interpolation=interpolation, transmission=transmission,
-    #                                       collapse='median', nwalkers=nwalkers_ini, bounds=bounds, a=2.0,
-    #                                       ac_c=ac_c, mu_sigma=mu_sigma,
-    #                                       burnin=0.3, rhat_threshold=1.01, rhat_count_threshold=1, conv_test=conv_test,
-    #                                       niteration_min=niteration_min, niteration_limit=niteration_limit,
-    #                                       niteration_supp=0, check_maxgap=20, nproc=nproc, algo=algo,
-    #                                       output_dir=outpath_5.format(bin_fac, filt + '_' + fm + label_test),
-    #                                       output_file="MCMC_results", display=False, verbosity=2,
-    #                                       save=True)
+    def do_negfc(self,do_firstguess=True, guess_xy=(0,0),mcmc_negfc=True, algo=pca_annular, nwalkers_ini=120, niteration_min = 25,
+                 niteration_limit=10000, save_plot=True,verbose=True,debug=False):
+        """
+        xxx
+
+        Parameters:
+        ***********
+        guess : tuple
+            first estimate of the source location to be provdied to firstguess()
+        mcmc_negfc : bool
+            whether to run MCMC NEGFC sampling (computationally intensive)
+        algo : pca_annulus, pca_annular, pca. default pca_annular
+            select which routine to be used to model and subtract the stellar PSF
+        nwalkers : int, default 120
+            the number of Goodman & Weare 'walkers'
+        verbose : bool
+            prints more output when True
+
+        """
+
+        print("======= Starting NEGFC....=======")
+        if guess_xy==(0,0):
+            raise ValueError("Enter an approximate location into guess_xy!")
+
+        outpath_sub = self.outpath + "negfc/"
+
+        if not isdir(self.outpath):
+            os.system("mkdir " + self.outpath)
+        if not isdir(outpath_sub):
+            os.system("mkdir " + outpath_sub)
+
+        if verbose:
+            print('Input path is {}'.format(self.inpath))
+            print('Output path is {}'.format(outpath_sub))
+
+        source = self.dataset_dict['source']
+        tn_shift = -0.58  # Milli et al 2017, true North correction factor
+
+        ADI_cube_name = '{}_master_cube.fits'  # template name for input master cube
+        derot_ang_name = 'derot_angles.fits'  # template name for corresponding input derotation angles
+        psfn_name = "master_unsat_psf_norm.fits"  # normalised PSF
+        #flux_psf_name = "master_unsat-stellarpsf_fluxes.fits"  # flux in a FWHM aperture found in calibration
+        ADI_cube = open_fits(self.inpath + ADI_cube_name.format(source), verbose=verbose)
+        derot_angles = open_fits(self.inpath + derot_ang_name, verbose=verbose) + tn_shift
+        psfn = open_fits(self.inpath + psfn_name, verbose=verbose)
+        #starphot = open_fits(self.inpath + flux_psf_name, verbose=verbose)[1]  # scaled fwhm flux is the second entry
+
+        if algo == pca_annular:
+            label_pca = 'pca_annular'
+        elif algo == pca_annulus:
+            label_pca = 'pca_annulus'
+        elif algo == pca:
+            label_pca = 'pca'
+        opt_npc = 15
+        ap_rad = 1 * self.fwhm
+        f_range = np.geomspace(0.1, 201, 40)
+        #weights = starphot / np.median(starphot) # not needed because we don't have filters?
+
+        if not isfile(outpath_sub+label_pca+"_npc{}_simplex_results.fits".format(opt_npc)) and do_firstguess:
+            ini_state = firstguess(ADI_cube, derot_angles, psfn, ncomp=opt_npc, plsc=self.pixel_scale,
+                                   planets_xy_coord=guess_xy, fwhm=self.fwhm,
+                                   annulus_width=12, aperture_radius=ap_rad, cube_ref=None,
+                                   svd_mode='lapack', scaling=None, fmerit='stddev', imlib='opencv',
+                                   interpolation='lanczos4', collapse='median', p_ini=None,
+                                   transmission=None, algo=pca_annular,
+                                   f_range=f_range, simplex=True, simplex_options=None, plot=False,
+                                   verbose=verbose, save=save_plot)
+            ini_state = np.array([ini_state[0][0], ini_state[1][0], ini_state[2][0]])
+
+            write_fits(outpath_sub+label_pca+"_npc{}_simplex_results.fits".format(opt_npc), ini_state,verbose=verbose)
+
+        if not isfile(outpath_sub + "MCMC_results") and mcmc_negfc:
+            ini_state = open_fits(outpath_sub + label_pca + "_npc{}_simplex_results.fits".format(opt_npc), verbose=verbose)
+
+            cy, cx = frame_center(ADI_cube[0])
+            dy_pl = guess_xy[0][1] - cy
+            dx_pl = guess_xy[0][0] - cx
+            r_pl = np.sqrt(np.power(dx_pl, 2) + np.power(dy_pl, 2))
+            theta_pl = (np.rad2deg(np.arctan2(dy_pl, dx_pl))) % 360
+            print("Estimated (r, PA) = ({:.1f},{:.1f})".format(r_pl, theta_pl))
+            delta_theta_min = np.rad2deg(np.arctan(4./r_pl)) #at least the angle corresponding to 2 azimuthal pixels
+            delta_theta = max(delta_theta_min,5.)
+            asize = 3 * self.fwhm
+            bounds = [(max(r_pl - asize / 2., 1), r_pl + asize / 2.),  # radius
+                      (theta_pl - delta_theta, theta_pl + delta_theta),  # angle
+                      (0, 5 * abs(ini_state[2]))]
+            weights = None
+
+            mcmc_negfc_sampling(ADI_cube, derot_angles, psfn, ncomp=opt_npc, plsc=self.pixel_scale,
+                                          initial_state=ini_state, fwhm=self.fwhm, weights=weights,
+                                          annulus_width=12, aperture_radius=ap_rad, cube_ref=None,
+                                          svd_mode='lapack', scaling=None, fmerit='stddev',
+                                          imlib='opencv', interpolation='lanczos4', transmission=None,
+                                          collapse='median', nwalkers=nwalkers_ini, bounds=bounds, a=2.0,
+                                          ac_c=50, mu_sigma=(0, 1),
+                                          burnin=0.3, rhat_threshold=1.01, rhat_count_threshold=1, conv_test='ac', # use autocorrelation to ensure sufficient sampling. sample around the aea of best likelihood to make distribution
+                                          niteration_min=niteration_min, niteration_limit=niteration_limit,
+                                          niteration_supp=0, check_maxgap=50, nproc=self.nproc, algo=algo,
+                                          output_dir=outpath_sub,
+                                          output_file="MCMC_results", display=False, verbosity=2,
+                                          save=save_plot)
