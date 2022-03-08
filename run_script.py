f361d43edc33d3535d65e99d01f8fb58203f4f73
iainhammond <imham3@student.monash.edu>
2022-03-08 16:04:49 +1100

    readability improvements

--- run_script.py
+++ run_script.py
@@ -30,49 +30,49 @@ pixel_scale = 0.027208  # arcsecs per pixel, Launhardt et al. 2020, +/- 0.000008
 
 # ***************************************** PARAMETERS TO CHANGE *******************************************************
 
-# CQTau
-# source = 'CQTau'  # used in some saved filenames and plots, without spaces
-# details = '(NACO+AGPM)'  # info displayed in plots and figures
-# ndit_sci = [100]  # number of frames per science cube
-# ndit_sky = [100]  # number of frames per sky cube
-# ndit_unsat = [500, 400]  # number of frames in unsaturated cubes
-# dit_sci = 0.35  # integration time for science frames
-# dit_unsat = 0.05  # integration time for unsaturated non coronagraphic images
-# dit_flat = 0.2  # integration time for flat frames
-# fast_reduction = False  # for super fast calibration and pre-processing (median combines all science cubes into one cube)
-#
-# # dictionary to pass through the pipeline saving all the static dataset information. Can be ignored
-# dataset_dict = {'wavelength':wavelength,'size_telescope':size_telescope,'pixel_scale':pixel_scale, 'source': source,
-#                 'details': details,'ndit_sci': ndit_sci, 'ndit_sky':ndit_sky,'ndit_unsat':ndit_unsat,'dit_sci':dit_sci,
-#                 'dit_unsat':dit_unsat,'dit_unsat':dit_unsat,'dit_flat':dit_flat,'fast_reduction':fast_reduction}
+# HD 169142
+source = 'HD_169142'  # used in some saved filenames and plots, without spaces
+details = '(NACO+AGPM)'  # info displayed in plots and figures
+ndit_sci = [100]  # number of frames per science cube
+ndit_sky = [100]  # number of frames per sky cube
+ndit_unsat = [400]  # number of frames in unsaturated cubes
+dit_sci = 0.35  # integration time for science frames
+dit_unsat = 0.07  # integration time for unsaturated non coronagraphic images
+dit_flat = 0.2  # integration time for flat frames
+fast_reduction = False  # for super fast calibration and pre-processing (median combines all science cubes into one cube)
+
+# dictionary to pass through the pipeline saving all the static dataset information. Can be ignored
+dataset_dict = {'wavelength':wavelength,'size_telescope':size_telescope,'pixel_scale':pixel_scale, 'source': source,
+                'details': details,'ndit_sci': ndit_sci, 'ndit_sky':ndit_sky,'ndit_unsat':ndit_unsat,'dit_sci':dit_sci,
+                'dit_unsat':dit_unsat,'dit_unsat':dit_unsat,'dit_flat':dit_flat,'fast_reduction':fast_reduction}
 
 # ************************* Activate various functions and set inpath + outpaths ***************************************
 
-# clas = input_dataset('/home/ihammond/pd87_scratch/products/NACO_archive/10_CQTau/raw/',
-#                      '/home/ihammond/pd87_scratch/products/NACO_archive/10_CQTau/classified/', dataset_dict,coro = True)
-# clas.bad_columns()
-# clas.mk_dico()
-# clas.find_sky_in_sci_cube(plot = 'save')
-# clas.find_derot_angles()
+clas = input_dataset('/home/ihammond/pd87_scratch/products/NACO_archive/13_HD169142/raw/',
+                     '/home/ihammond/pd87_scratch/products/NACO_archive/13_HD169142/classified/', dataset_dict,coro=True)
+clas.bad_columns()
+clas.mk_dico()
+clas.find_sky_in_sci_cube(plot='save')
+clas.find_derot_angles()
 
-# calib = raw_dataset('/home/ihammond/pd87_scratch/products/NACO_archive/10_CQTau/classified/',
-#                     '/home/ihammond/pd87_scratch/products/NACO_archive/10_CQTau/calibrated/', dataset_dict,final_sz = None)
-# calib.dark_subtract(method='median', bad_quadrant = [3], debug = False, plot = 'save')
-# calib.flat_field_correction(debug = False, plot = 'save')
-# calib.correct_nan(debug = False, plot = 'save')
-# calib.correct_bad_pixels(debug = False, plot = 'save')
-# calib.first_frames_removal(nd_filter = False, verbose = True, debug = False, plot = 'save')
-# calib.get_stellar_psf(nd_filter = False, debug = False, plot = 'save')
-# calib.subtract_sky(npc = 1, debug = False, plot = 'save')
+calib = raw_dataset('/home/ihammond/pd87_scratch/products/NACO_archive/13_HD169142/classified/',
+                    '/home/ihammond/pd87_scratch/products/NACO_archive/13_HD169142/calibrated/', dataset_dict,final_sz=None)
+calib.dark_subtract(method='median', bad_quadrant=[3], debug=False, plot='save')
+calib.flat_field_correction(debug=False, plot='save')
+calib.correct_nan(debug=False, plot='save')
+calib.correct_bad_pixels(debug=False, plot='save')
+calib.first_frames_removal(verbose=True, debug=False, plot='save')
+calib.get_stellar_psf(nd_filter=False, debug=False, plot='save')
+calib.subtract_sky(npc=1, debug=False, plot='save')
 
-# preproc = calib_dataset('/home/ihammond/pd87_scratch/products/NACO_archive/10_CQTau/calibrated/',
-#                         '/home/ihammond/pd87_scratch/products/NACO_archive/10_CQTau/preproc/', dataset_dict,
-#                         recenter_method='speckle', recenter_model='gauss', coro=True)
-# preproc.recenter(nproc=nproc, sigfactor=4, subi_size=41, crop_sz=251, verbose=True, debug=False, plot=True, coro=True)
-# preproc.bad_frame_removal(pxl_shift_thres=0.4, sub_frame_sz=31, verbose=True, debug=False, plot=True)
-# preproc.crop_cube(arcsecond_diameter=3, verbose=True, debug=False)  # required for PCA-ADI annular and contrast curves
-# preproc.median_binning(binning_factor=1, verbose=True)  # speeds up PCA-ADI annular and contrast curves, reduces S/N
-#
+preproc = calib_dataset('/home/ihammond/pd87_scratch/products/NACO_archive/10_CQTau/calibrated/',
+                        '/home/ihammond/pd87_scratch/products/NACO_archive/10_CQTau/preproc/', dataset_dict,
+                        recenter_method='speckle', recenter_model='gauss', coro=True)
+preproc.recenter(nproc=nproc, sigfactor=4, subi_size=41, crop_sz=251, verbose=True, debug=False, plot=True, coro=True)
+preproc.bad_frame_removal(pxl_shift_thres=0.4, sub_frame_sz=31, verbose=True, debug=False, plot=True)
+preproc.crop_cube(arcsecond_diameter=3, verbose=True, debug=False)  # required for PCA-ADI annular and contrast curves
+preproc.median_binning(binning_factor=1, verbose=True)  # speeds up PCA-ADI annular and contrast curves, reduces S/N
+
 # postproc = preproc_dataset('/home/ihammond/pd87_scratch/products/NACO_archive/10_CQTau/preproc/',
 #                             '/home/ihammond/pd87_scratch/products/NACO_archive/10_CQTau/postproc_nobin/',
 #                            dataset_dict, nproc=nproc, npc=20) # npc can be int (for a single number of PCs), tuple
@@ -87,7 +87,7 @@ pixel_scale = 0.027208  # arcsecs per pixel, Launhardt et al. 2020, +/- 0.000008
 
 # some previous data sets:
 
-#MWC480
+# MWC480
 # source = 'MWC480' # used in some saved filenames and plots
 # details = '(NACO+AGPM)' # info displayed in plots and figures
 # ndit_sci = [100] #number of frames per science cube
@@ -98,7 +98,7 @@ pixel_scale = 0.027208  # arcsecs per pixel, Launhardt et al. 2020, +/- 0.000008
 # dit_flat = 0.2 #integration time for flat frames in seconds
 # fast_reduction = False # for super fast calibration and pre-processing (median combines all science cubes into one cube)
 
-#HD179218
+# HD179218
 #source = 'HD179218' # used in some saved filenames and plots
 #details = '(NACO+AGPM)' # info displayed in plots and figures
 #dit_sci = 0.35 #integration time for sci
@@ -107,7 +107,7 @@ pixel_scale = 0.027208  # arcsecs per pixel, Launhardt et al. 2020, +/- 0.000008
 #dit_unsat = 0.07 #integration time for unsaturated non coronagraphic images
 #ndit_unsat = [400] #number of unsaturated frames in cubes
 
-#HD206893
+# HD206893
 # source = 'HD206893' # used in some saved filenames and plots
 # details = '(NACO+AGPM)' # info displayed in plots and figures
 # ndit_sci = [200] #number of frames per science cube
@@ -116,3 +116,14 @@ pixel_scale = 0.027208  # arcsecs per pixel, Launhardt et al. 2020, +/- 0.000008
 # dit_sci = 0.3 #integration time for science frames
 # dit_unsat = 0.1 #integration time for unsaturated non coronagraphic images
 # dit_flat = 0.2 #integration time for flat frames
+
+# CQTau
+# source = 'CQTau'  # used in some saved filenames and plots, without spaces
+# details = '(NACO+AGPM)'  # info displayed in plots and figures
+# ndit_sci = [100]  # number of frames per science cube
+# ndit_sky = [100]  # number of frames per sky cube
+# ndit_unsat = [500, 400]  # number of frames in unsaturated cubes
+# dit_sci = 0.35  # integration time for science frames
+# dit_unsat = 0.05  # integration time for unsaturated non coronagraphic images
+# dit_flat = 0.2  # integration time for flat frames
+# fast_reduction = False  # for super fast calibration and pre-processing (median combines all science cubes into one cube)
